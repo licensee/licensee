72fa8653d07cf0f0b2a755252e59320fe7089ffc
Ben Balter <ben.balter@github.com>
2015-09-08 12:29:10 -0400

    reorder license preference

--- lib/licensee/project_file.rb
+++ lib/licensee/project_file.rb
@@ -80,15 +80,17 @@ class Licensee
       # filename - (string) the name of the file to score
       #
       # Returns 1.0  if the file is definitely a license file (e.g, LICENSE)
-      # Returns 0.9  if the file is almost certainly a license file (e.g., LICENSE.md)
-      # Returns 0.75 if the file is probably a license file (e.g., COPYING, COPYING.md)
+      # Returns 0.9 if the file is almost certainly a license file (e.g., LICENSE.md)
+      # Returns 0.8 if the file is probably a license file (e.g., COPYING, COPYING.md)
+      # Returns 0.9  if the file is potentially a license file (e.g., LICENSE.php)
       # Returns 0.5  if the file is likely a license file (MIT-LICENSE)
       # Returns 0.0  if the file is definitely not a license file (e.g., index.php)
       def license_score(filename)
-        return 1.0  if filename =~ /\A(un)?licen[sc]e\z/i
-        return 0.9  if filename =~ /\A(un)?licen[sc]e(\.[^.]+)\z/i
-        return 0.75 if filename =~ /\Acopy(ing|right)(\.[^.]+)?\z/i
-        return 0.5  if filename =~ /licen[sc]e/i
+        return 1.0 if filename =~ /\A(un)?licen[sc]e\z/i
+        return 0.9 if filename =~ /\A(un)?licen[sc]e\.(md|markdown|txt)\z/i
+        return 0.8 if filename =~ /\Acopy(ing|right)(\.[^.]+)?\z/i
+        return 0.7 if filename =~ /\A(un)?licen[sc]e\.[^.]+\z/i
+        return 0.5 if filename =~ /licen[sc]e/i
         return 0.0
       end
     end
