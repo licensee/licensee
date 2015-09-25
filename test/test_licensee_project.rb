baa38da1f3dd5222857a9f7dd9db45dd7d345f8e
Vicent Marti <tanoku@gmail.com>
2015-09-25 08:50:09 -0700

    Test GitProject and FSProject the same

--- test/test_licensee_project.rb
+++ test/test_licensee_project.rb
@@ -2,50 +2,68 @@ require 'helper'
 require 'fileutils'
 
 class TestLicenseeProject < Minitest::Test
-  describe("git repository project") do
-    def make_project(fixture_name)
-      fixture = fixture_path fixture_name
-      Licensee::GitProject.new(fixture)
-    end
 
-    should "detect the license file" do
-      project = make_project "licenses.git"
-      assert_instance_of Licensee::Project::LicenseFile, project.license_file
-    end
+  ["git", "filesystem"].each do |project_type|
+    describe("#{project_type} repository project") do
+      if project_type == "git"
+        def make_project(fixture_name)
+          fixture = fixture_path fixture_name
+          Licensee::GitProject.new(fixture)
+        end
+      else
+        def make_project(fixture_name)
+          dest = File.join("tmp", "fixtures", fixture_name)
+          FileUtils.mkdir_p File.dirname(dest)
+          system "git", "clone", "-q", fixture_path(fixture_name), dest
+          FileUtils.rm_r File.join(dest, ".git")
 
-    should "detect the license" do
-      project = make_project "licenses.git"
-      assert_equal "mit", project.license.key
-    end
+          Licensee::FSProject.new(dest)
+        end
 
-    should "detect an atypically cased license file" do
-      project = make_project "case-sensitive.git"
-      assert_instance_of Licensee::Project::LicenseFile, project.license_file
-    end
+        def teardown
+          FileUtils.rm_rf "tmp/fixtures" 
+        end
+      end
 
-    should "detect MIT-LICENSE licensed projects" do
-      project = make_project "named-license-file-prefix.git"
-      assert_equal "mit", project.license.key
-    end
+      should "detect the license file" do
+        project = make_project "licenses.git"
+        assert_instance_of Licensee::Project::LicenseFile, project.license_file
+      end
 
-    should "detect LICENSE-MIT licensed projects" do
-      project = make_project "named-license-file-suffix.git"
-      assert_equal "mit", project.license.key
-    end
+      should "detect the license" do
+        project = make_project "licenses.git"
+        assert_equal "mit", project.license.key
+      end
 
-    should "not error out on repos with folders names license" do
-      project = make_project "license-folder.git"
-      assert_equal nil, project.license
-    end
+      should "detect an atypically cased license file" do
+        project = make_project "case-sensitive.git"
+        assert_instance_of Licensee::Project::LicenseFile, project.license_file
+      end
 
-    should "detect licence files" do
-      project = make_project "licence.git"
-      assert_equal "mit", project.license.key
-    end
+      should "detect MIT-LICENSE licensed projects" do
+        project = make_project "named-license-file-prefix.git"
+        assert_equal "mit", project.license.key
+      end
 
-    should "detect an unlicensed project" do
-      project = make_project "no-license.git"
-      assert_equal nil, project.license
+      should "detect LICENSE-MIT licensed projects" do
+        project = make_project "named-license-file-suffix.git"
+        assert_equal "mit", project.license.key
+      end
+
+      should "not error out on repos with folders names license" do
+        project = make_project "license-folder.git"
+        assert_equal nil, project.license
+      end
+
+      should "detect licence files" do
+        project = make_project "licence.git"
+        assert_equal "mit", project.license.key
+      end
+
+      should "detect an unlicensed project" do
+        project = make_project "no-license.git"
+        assert_equal nil, project.license
+      end
     end
   end
 
