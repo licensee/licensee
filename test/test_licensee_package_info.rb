4d938c585bd888a86625f2f8420d8ae03aa84118
Ben Balter <ben.balter@github.com>
2015-11-26 16:27:02 -0600

    whitespace

--- test/test_licensee_package_info.rb
+++ test/test_licensee_package_info.rb
@@ -3,10 +3,10 @@ require 'helper'
 class TestLicenseePackageInfo < Minitest::Test
   context "license filename scoring" do
     EXPECTATIONS = {
-      "licensee.gemspec"     => 1.0,
-      "package.json" => 1.0,
-      "bower.json"   => 0.75,
-      "README.md"    => 0.0
+      "licensee.gemspec" => 1.0,
+      "package.json"     => 1.0,
+      "bower.json"       => 0.75,
+      "README.md"        => 0.0
     }
 
     EXPECTATIONS.each do |filename, expected|
