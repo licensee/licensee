6505c9b82b470b1f60f0bb925de0da48c2a0f418
Ben Balter <ben.balter@github.com>
2016-09-13 11:33:35 -0400

    test the content matcher

---
+++ test/licensee/test_content_helper.rb
@@ -0,0 +1,38 @@
+require 'helper'
+
+class ContentHelperTestHelper
+  include Licensee::ContentHelper
+  attr_accessor :content
+
+  DEFAULT_CONTENT = <<-EOS
+Copyright 2016 Ben Balter
+
+The made up license.
+  EOS
+
+  def initialize(content=nil)
+    @content = content || DEFAULT_CONTENT
+  end
+end
+
+class TestLicenseeContentHelper < Minitest::Test
+  def setup
+    @helper = ContentHelperTestHelper.new
+  end
+
+  should "normalize the content" do
+    assert_equal "the made up license.", @helper.content_normalized
+  end
+
+  should "generate the hash" do
+    assert_equal "3c59634b9fae4396a76a978f3f6aa718ed790a9a", @helper.hash
+  end
+
+  should "calculate the length" do
+    assert_equal 20, @helper.length
+  end
+
+  should "build the wordset" do
+    assert_equal %w[the made up license].to_set, @helper.wordset
+  end
+end
