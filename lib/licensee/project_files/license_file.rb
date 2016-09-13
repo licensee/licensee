5da8c7f7d51daa9f2a2c5c004104e378c1f37de2
Waldir Pimenta <waldyrious@gmail.com>
2016-09-13 16:12:23 +0100

    score LICENSE.md et al. over COPYING/COPYRIGHT

--- lib/licensee/project_files/license_file.rb
+++ lib/licensee/project_files/license_file.rb
@@ -14,8 +14,8 @@ module Licensee
 
       def self.name_score(filename)
         return 1.0 if filename =~ /\A(un)?licen[sc]e\z/i
-        return 0.9 if filename =~ /\Acopy(ing|right)\z/i
-        return 0.8 if filename =~ /\A(un)?licen[sc]e\.(md|markdown|txt)\z/i
+        return 0.9 if filename =~ /\A(un)?licen[sc]e\.(md|markdown|txt)\z/i
+        return 0.8 if filename =~ /\Acopy(ing|right)\z/i
         return 0.7 if filename =~ /\Acopy(ing|right)\.(md|markdown|txt)\z/i
         return 0.6 if filename =~ /\A(un)?licen[sc]e\.[^.]+\z/i
         return 0.5 if filename =~ /\Acopy(ing|right)\.[^.]+\z/i
