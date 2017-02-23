2202ad4f4600053808d3f6fdbddbc5bb9aaa8cb7
Ben Balter <ben.balter@github.com>
2017-02-23 14:42:57 -0500

    anchor copyright regex to start of file

--- lib/licensee/project_files/license_file.rb
+++ lib/licensee/project_files/license_file.rb
@@ -41,7 +41,7 @@ module Licensee
 
       def attribution
         @attribution ||= begin
-          matches = /\A#{Matchers::Copyright::REGEX}$/i
+          matches = Matchers::Copyright::REGEX
                     .match(content_without_title_and_version)
           matches[0] if matches
         end
