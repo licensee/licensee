533c63af20ab2c8340f3c9b4bd1b7853098906db
Mike Linksvayer <mlinksva@github.com>
2017-12-30 14:37:57 -0800

    rubocop

--- lib/licensee/projects/github_project.rb
+++ lib/licensee/projects/github_project.rb
@@ -12,7 +12,8 @@ module Licensee
     class GitHubProject < Licensee::Projects::Project
       # If there's any trailing data (e.g. `.git`) this pattern will ignore it:
       # we're going to use the API rather than clone the repo.
-      GITHUB_REPO_PATTERN = %r{https://github.com/([^\/]+\/([^\/]+(?=\.git)|[^\/]+)).*}
+      GITHUB_REPO_PATTERN =
+        %r{https://github.com/([^\/]+\/([^\/]+(?=\.git)|[^\/]+)).*}
 
       class RepoNotFound < StandardError; end
 
