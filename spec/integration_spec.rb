a783912eaf387a3269a7f674c20752b538aab3a0
Mike Linksvayer <mlinksva@github.com>
2020-08-03 16:20:53 -0700

    fix my bad conflict resolution

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -270,9 +270,8 @@ RSpec.describe 'integration test' do
             expect(subject.license).to eql(license)
           end
         end
-      end
       
-      context 'MIT with byte order mark' do
+        context 'MIT with byte order mark' do
           let(:license) { Licensee::License.find('mit') }
           let(:fixture) { 'bom' }
 
