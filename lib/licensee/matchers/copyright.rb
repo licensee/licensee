0d8b70974ee474db4a47ba4e3f9442bb7cae186c
Mike Linksvayer <mlinksva@github.com>
2020-12-24 23:03:51 -0800

    auto correct Lint/RedundantCopDisableDirective

--- lib/licensee/matchers/copyright.rb
+++ lib/licensee/matchers/copyright.rb
@@ -5,11 +5,8 @@ module Licensee
     class Copyright < Licensee::Matchers::Matcher
       attr_reader :file
 
-      # rubocop:disable Layout/LineLength
       COPYRIGHT_SYMBOLS = Regexp.union([/copyright/i, /\(c\)/i, "\u00A9", "\xC2\xA9"])
       REGEX = /#{ContentHelper::START_REGEX}(?:portions )?([_*\-\s]*#{COPYRIGHT_SYMBOLS}.*$)+$/i.freeze
-      # rubocop:enable Layout/LineLength
-
       def match
         # NOTE: must use content, and not content_normalized here
         if file.content.strip =~ /#{REGEX}+\z/io
