33fddd6115a053ff0fa84b76f694d127c9f2d472
bmarjanovic <bmarjanovic@gitlab.com>
2023-11-17 13:52:33 +0100

    License-path command
    Fixes #667

--- lib/licensee/commands/license_path.rb
+++ lib/licensee/commands/license_path.rb
@@ -2,15 +2,15 @@
 
 class LicenseeCLI < Thor
   desc 'license-path [PATH]', "Returns the path to the given project's license file"
-  def license_path(_path)
-    if project.license_file
-      if remote?
-        say project.license_file.path
-      else
-        say File.expand_path project.license_file.path
-      end
+  def license_path(path)
+    project = Licensee.project(path)
+
+    exit 1 unless project.license_file
+
+    if remote?
+      say project.license_file.path
     else
-      exit 1
+      say File.expand_path(project.license_file.path, path)
     end
   end
 end
