5897e2a9878c76907397cd432854903a2c7e80fe
Jarryd Lee <jarryd@semgrep.com>
2024-03-11 14:52:03 -0700

    Update ref tests
    Refactored the ref tests to correctly use subject over instance. Also
    moved the tests into a better context that fits better.

--- spec/licensee/projects/git_hub_project_spec.rb
+++ spec/licensee/projects/git_hub_project_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Licensee::Projects::GitHubProject do
   let(:repo) { 'benbalter/licensee' }
   let(:github_url) { "https://github.com/#{repo}" }
   let(:mit) { Licensee::License.find('mit') }
-  let(:readme_file) { File.read(fixture_path('mit/README.md')) }
-  let(:license_file) { File.read(fixture_path('mit/LICENSE.txt')) }
+  let(:mit_readme_file) { File.read(fixture_path('mit/README.md')) }
+  let(:mit_license_file) { File.read(fixture_path('mit/LICENSE.txt')) }
+  let(:apache2) { Licensee::License.find('apache-2.0') }
+  let(:apache2_license_file) { File.read(fixture_path('apache-with-readme-notice/LICENSE')) }
 
   describe '#initialize' do
     context 'with a GitHub URI' do
@@ -52,11 +54,11 @@ RSpec.describe Licensee::Projects::GitHubProject do
 
       stub_request(:get, 'https://api.github.com/repos/benbalter/licensee/contents/LICENSE.txt')
         .with(headers: { 'accept' => 'application/vnd.github.v3.raw' })
-        .to_return(status: 200, body: license_file)
+        .to_return(status: 200, body: mit_license_file)
 
       stub_request(:get, 'https://api.github.com/repos/benbalter/licensee/contents/README.md')
         .with(headers: { 'accept' => 'application/vnd.github.v3.raw' })
-        .to_return(status: 200, body: readme_file)
+        .to_return(status: 200, body: mit_readme_file)
     end
 
     it 'returns the license' do
@@ -95,6 +97,36 @@ RSpec.describe Licensee::Projects::GitHubProject do
         expect(subject.license).to eql(mit)
       end
     end
+
+    context 'when initialized with a ref' do
+      subject { described_class.new(github_url, ref: 'my-ref') }
+
+      before do
+        stub_request(:get, 'https://api.github.com/repos/benbalter/licensee/contents/?ref=my-ref')
+          .to_return(
+            status:  200,
+            body:    fixture_contents('webmock/licensee_alternate_ref.json'),
+            headers: { 'Content-Type' => 'application/json' }
+          )
+
+        stub_request(:get, 'https://api.github.com/repos/benbalter/licensee/contents/LICENSE?ref=my-ref')
+          .with(headers: { 'accept' => 'application/vnd.github.v3.raw' })
+          .to_return(status: 200, body: apache2_license_file)
+      end
+
+      it 'returns the ref' do
+        expect(subject.ref).to eql('my-ref')
+      end
+
+      it 'returns query params' do
+        expect(subject.send(:query_params)).to eql({ ref: subject.ref })
+      end
+
+      it 'returns the license' do
+        expect(subject.license).to be_a(Licensee::License)
+        expect(subject.license).to eql(apache2)
+      end
+    end
   end
 
   context 'when the repo cannot be found' do
@@ -107,22 +139,6 @@ RSpec.describe Licensee::Projects::GitHubProject do
 
     it 'raises a RepoNotFound error' do
       expect { subject.license }.to raise_error(described_class::RepoNotFound)
-    end
-  end
-
-  context 'when initialized with a ref' do
-    let(:ref) { 'my-ref' }
-
-    before do
-      instance.instance_variable_set(:@ref, 'my-ref')
-    end
-
-    it 'returns the ref' do
-      expect(instance.ref).to eql(ref)
-    end
-
-    it 'returns query params' do
-      expect(instance.send(:query_params)).to eql({ ref: instance.ref })
     end
   end
 end
