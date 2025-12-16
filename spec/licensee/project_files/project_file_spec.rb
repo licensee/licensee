3d91e693a81ad2ef6aa6f523f93380317dbe76cb
Mike Linksvayer <mlinksva@github.com>
2025-12-16 23:48:16 +0000

    Fix RSpec/SubjectStub in specs
    Avoid stubbing the subject by using a small matcher subclass in package_matcher_spec and by relying on real LicenseFile behavior in project_file_spec; remove the cop exclusion from .rubocop_todo.yml.

--- spec/licensee/project_files/project_file_spec.rb
+++ spec/licensee/project_files/project_file_spec.rb
@@ -6,15 +6,6 @@ RSpec.describe Licensee::ProjectFiles::ProjectFile do
   let(:filename) { 'LICENSE.txt' }
   let(:mit) { Licensee::License.find('mit') }
   let(:content) { mit.content }
-  let(:possible_matchers) { [Licensee::Matchers::Exact] }
-
-  before do
-    allow(subject).to receive(:possible_matchers).and_return(possible_matchers)
-  end
-
-  before { allow(subject).to receive(:length).and_return(mit.length) }
-
-  before { allow(subject).to receive(:wordset).and_return(mit.wordset) }
 
   it 'stores the content' do
     expect(subject.content).to eql(content)
